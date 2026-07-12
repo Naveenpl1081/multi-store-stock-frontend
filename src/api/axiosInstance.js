d7c53a8778f928b5d4c86232c87b0946f20601e9
Naveenpl1081 <123857457+Naveenpl1081@users.noreply.github.com>
2026-07-12 13:08:29 +0530

    style: clean up formatting and remove dead code

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-
-
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
@@ -12,7 +10,6 @@ const axiosInstance = axios.create({
 console.log("Vite Env Variables:", import.meta.env);
 
 
-// attach the token to every outgoing request, if present
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -21,7 +18,7 @@ axiosInstance.interceptors.request.use((config) => {
   return config;
 });
 
-// if the token is invalid/expired, clear it and force re-login
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
